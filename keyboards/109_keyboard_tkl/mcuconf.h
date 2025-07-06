7fa072a52dec3e66a89f7d48f7b7f7a9f02209e7
DanysP <petras.danys@gmail.com>
2025-07-06 12:51:54 +0100

    Added ChibiOS HAL settings to enable GPIO ports in mcuconf.h

---
+++ keyboards/109_keyboard_tkl/mcuconf.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For adding or overriding other ChibiOS HAL settings if needed
+
+#define PAL_USE_PORTA TRUE // Enables Ports A for GPIO operations
+#define PAL_USE_PORTB TRUE // Enables Ports B for GPIO operations
+#define PAL_USE_PORTC TRUE // Enables Ports C for GPIO operations
+#define PAL_USE_PORTG TRUE // Enables Ports G for GPIO operations
